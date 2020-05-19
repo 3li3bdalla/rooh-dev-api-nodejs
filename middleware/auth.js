328ab6c63fd63241c6a285771d6d1bdeb4ce195d
3li3bdalla <ali.dev.sd@gmail.com>
2020-05-19 06:13:25 +0200

    update has been made

---
+++ middleware/auth.js
@@ -0,0 +1,104 @@
+var Models = require('../models');
+var Constants = require('../config/appConstants');
+var RESPONSE_MESSAGES = require('../config/response-messages')
+var jwt = require("jsonwebtoken");
+var multer = require('multer');
+
+async function decryptData(data) {
+    if (!data.encryptionKey) return {};
+    await rsaWrapper.initLoadKeys(__dirname + '/../utils');
+    var decryptedKey = await rsaWrapper.decrypt(rsaWrapper.serverPrivate, data.encryptionKey);
+    /*let descryptedPayload = JSON.parse(await aesWrapper.decrypt(Buffer.from(decryptedKey, 'base64'), data.encryptionData));*/
+
+    let descryptedPayload = {};
+    /*if(data.type && data.type=="web"){
+        global.encryptionType = "web";
+        descryptedPayload = await aesWrapper.webDecrypt(Buffer.from(decryptedKey, 'base64'), data.encryptionData);
+    }else{*/
+    descryptedPayload = JSON.parse(await aesWrapper.decrypt(Buffer.from(decryptedKey, 'base64'), data.encryptionData));
+    //}
+    return descryptedPayload;
+}
+
+
+
+exports.validateToken = function (req, res, next) {
+    global.encryptionType = "mobile";
+    let token = req.headers['authorization'];
+    let language = "en";
+    if (req.headers['language'] != undefined && req.headers['language'] != "") {
+        language = req.headers['language'];
+    }
+    if (token) {
+        let authToken = token.split(" ")
+        console.log("-------", authToken[1]);
+
+        jwt.verify(authToken[1], gRouter.get(Constants.SERVER.JWT_SECRET_KEY).toString(), async function (err, decoded) {
+            if (err) {
+                if (authToken[1] == Constants.SERVER.GUEST_TOKEN) {
+
+                    //DESCRYPTION
+                    if (process.env.ENABLE_ENCRYPTION == "1") {
+                        if (req.body != undefined) {
+                            req.body = await decryptData(req.body);
+                        }
+                        if (req.query != undefined) {
+                            req.query = await decryptData(req.query);
+                        }
+                    }
+                    //DESCRYPTION
+                    global.userData = "";
+                    next();
+                } else {
+                    res.status(401).json({
+                        status: 0,
+                        message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.ERROR_TOKEN_AUTH.customMessage
+                    });
+                    return;
+                }
+            } else {
+                //DESCRYPTION
+                if (process.env.ENABLE_ENCRYPTION == "1") {
+                    if (req.body != undefined) {
+                        req.body = await decryptData(req.body);
+                    }
+                    if (req.query != undefined) {
+                        req.query = await decryptData(req.query);
+                    }
+                }
+                //DESCRYPTION
+                Models.Users.findOne({
+                    "_id": decoded._id
+                }, {}, function (err, result) {
+                    if (!result || (result.accessToken == "") || (result.accessToken != authToken[1])) return res.status(401).json({
+                        status: 0,
+                        message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.ERROR_TOKEN_AUTH.customMessage
+                    });
+
+                    if (result.isBlocked == true) {
+                        return res.status(403).json({
+                            status: 0,
+                            message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.BLOCK_USER.customMessage
+                        });
+                    } else if (result.isDeleted == true) {
+                        return res.status(403).json({
+                            status: 0,
+                            message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.NO_USER_EXISTS.customMessage
+                        });
+                    } else {
+                        global.userData = result;
+                        global.language = language;
+                        req.credentials = result;
+                        next();
+                    }
+                });
+            }
+        });
+    } else {
+        res.status(403).json({
+            status: 0,
+            message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.EMPTY_TOKEN.customMessage
+        });
+        return;
+    }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require("jsonwebtoken");
-var multer = require('multer');
 var user = require('../controllers/userControllers');
-var Models = require('../models');
-var Constants = require('../config/appConstants');
-var RESPONSE_MESSAGES = require('../config/response-messages')
+const auth = require('../middleware/auth');
 
 //DESCRYPTION
 if (process.env.ENABLE_ENCRYPTION == "1") {
@@ -13,86 +9,7 @@ if (process.env.ENABLE_ENCRYPTION == "1") {
   var rsaWrapper = require('../Lib/rsa-wrapper');
 }
 //DESCRYPTION
-
-router.use(function (req, res, next) {
-  global.encryptionType = "mobile";
-  let token = req.headers['authorization'];
-  console.log("-------", token)
-  let language = "en";
-  if (req.headers['language'] != undefined && req.headers['language'] != "") {
-    language = req.headers['language'];
-  }
-  if (token) {
-    let authToken = token.split(" ")
-    jwt.verify(authToken[1], gRouter.get(Constants.SERVER.JWT_SECRET_KEY).toString(), async function (err, decoded) {
-      if (err) {
-        if (authToken[1] == Constants.SERVER.GUEST_TOKEN) {
-
-          //DESCRYPTION
-          if (process.env.ENABLE_ENCRYPTION == "1") {
-            if (req.body != undefined) {
-              req.body = await decryptData(req.body);
-            }
-            if (req.query != undefined) {
-              req.query = await decryptData(req.query);
-            }
-          }
-          //DESCRYPTION
-          global.userData = "";
-          next();
-        } else {
-          res.status(401).json({
-            status: 0,
-            message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.ERROR_TOKEN_AUTH.customMessage
-          });
-          return;
-        }
-      } else {
-        //DESCRYPTION
-        if (process.env.ENABLE_ENCRYPTION == "1") {
-          if (req.body != undefined) {
-            req.body = await decryptData(req.body);
-          }
-          if (req.query != undefined) {
-            req.query = await decryptData(req.query);
-          }
-        }
-        //DESCRYPTION
-        Models.Users.findOne({
-          "_id": decoded._id
-        }, {}, function (err, result) {
-          if (!result || (result.accessToken == "") || (result.accessToken != authToken[1])) return res.status(401).json({
-            status: 0,
-            message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.ERROR_TOKEN_AUTH.customMessage
-          });
-
-          if (result.isBlocked == true) {
-            return res.status(403).json({
-              status: 0,
-              message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.BLOCK_USER.customMessage
-            });
-          } else if (result.isDeleted == true) {
-            return res.status(403).json({
-              status: 0,
-              message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.NO_USER_EXISTS.customMessage
-            });
-          } else {
-            global.userData = result;
-            global.language = language;
-            req.credentials = result;
-            next();
-          }
-        });
-      }
-    });
-  } else {
-    res.status(403).json({
-      status: 0,
-      message: RESPONSE_MESSAGES.STATUS_MSG.ERROR.EMPTY_TOKEN.customMessage
-    });
-    return;
-  }
-});
+router.use(auth.validateToken);
 
 router.post('/updateProfile', user.updateProfile);
 router.post('/updateProfessionalProfile', user.updateProfessionalProfile);
@@ -151,20 +68,6 @@ router.get('/getPrescriptionsList', user.getPrescriptionsList);
 router.post('/testapiSave', user.testapiSave);
 router.get('/testapiGet', user.testapiGet);
 
-async function decryptData(data) {
-  if (!data.encryptionKey) return {};
-  await rsaWrapper.initLoadKeys(__dirname + '/../utils');
-  var decryptedKey = await rsaWrapper.decrypt(rsaWrapper.serverPrivate, data.encryptionKey);
-  /*let descryptedPayload = JSON.parse(await aesWrapper.decrypt(Buffer.from(decryptedKey, 'base64'), data.encryptionData));*/
 
-  let descryptedPayload = {};
-  /*if(data.type && data.type=="web"){
-      global.encryptionType = "web";
-      descryptedPayload = await aesWrapper.webDecrypt(Buffer.from(decryptedKey, 'base64'), data.encryptionData);
-  }else{*/
-  descryptedPayload = JSON.parse(await aesWrapper.decrypt(Buffer.from(decryptedKey, 'base64'), data.encryptionData));
-  //}
-  return descryptedPayload;
-}
 
 module.exports = router;
